c7deedd4d62f2d4dcc047ae2ac960e6561770453
Saiful Nakeusa <snakeusa8@gmail.com>
2020-07-30 15:23:39 +0700

    progress bar

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
 
@@ -29,6 +30,7 @@ const UploadForm = () => {
             <div className="output">
                 {error && <div className="error"> {error} </div>}
                 {file && <div> {file.name} </div>}
+                {file && <ProgressBar file={file} setFile={setFile} />}
             </div>
         </form>
     )
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectStorage } from "../firebase/config";
+import { projectStorage, projectFirestore, timestamp } from "../firebase/config";
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
@@ -9,14 +9,17 @@ const useStorage = (file) => {
     useEffect(() => {
         // references
         const storageRef = projectStorage.ref(file.name);
+        const collectionRef = projectFirestore.collection('images');
 
         storageRef.put(file).on('state_changed', (snap) => {
-            let percentage = (span.bytesTransferred / snap.totalBytes) * 100;
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
             setProgress(percentage)
         }, (err) => {
             setError(err)
         }, async () => {
             const url = await storageRef.getDownloadURL();
+            const createdAt = timestamp();
+            collectionRef.add({ url, createdAt });
             setUrl(url);
         });
     }, [file]);
